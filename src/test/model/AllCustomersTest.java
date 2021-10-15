db77c9ac4bb255f6311cba51454d56dab85ce3d8
Parmvir-S <parmvir.shergill@gmail.com>
2021-10-15 15:25:23 -0700

    Added some more tests

--- src/test/model/AllCustomersTest.java
+++ src/test/model/AllCustomersTest.java
@@ -3,6 +3,9 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AllCustomersTest {
 
     private AllCustomers allCustomers;
@@ -14,36 +17,80 @@ public class AllCustomersTest {
 
     @Test
     public void getCustomerTest() {
+        for (int i = 1; i <= 5; i++) {
+            allCustomers.addCustomer(new Customer(i,
+                    "#" + i,
+                    "",
+                    0,
+                    new CustomerCart(new ItemList())));
+        }
 
+        assertEquals(allCustomers.getAllCustomers().get(1), allCustomers.getCustomer("#2"));
+        assertEquals(allCustomers.getAllCustomers().get(0), allCustomers.getCustomer("#23"));
     }
 
     @Test
     public void addCustomerTest() {
+        assertEquals(0, allCustomers.getTotalNumberOfCustomers());
+        Customer cust1 = new Customer(6, "LeBron", "gmail", 360, new CustomerCart(new ItemList()));
+        allCustomers.addCustomer(cust1);
+        assertEquals(1, allCustomers.getTotalNumberOfCustomers());
 
+        Customer cust2 = new Customer(30, "Curry", "gmail", 11, new CustomerCart(new ItemList()));
+        allCustomers.addCustomer(cust2);
+        assertEquals(2, allCustomers.getTotalNumberOfCustomers());
     }
 
     @Test
     public void removeCustomerTest() {
-
-    }
-
-    @Test
-    public void getAllCustomersTest() {
-
-    }
-
-    @Test
-    public void getTotalNumberOfCustomersTest() {
-
+        for (int i = 1; i <= 5; i++) {
+            allCustomers.addCustomer(new Customer(i,
+                    "#" + i,
+                    "",
+                    0,
+                    new CustomerCart(new ItemList())));
+        }
+        assertEquals(5, allCustomers.getTotalNumberOfCustomers());
+        allCustomers.removeCustomer("#1");
+        assertEquals(4, allCustomers.getTotalNumberOfCustomers());
+        allCustomers.removeCustomer("#4");
+        assertEquals(3, allCustomers.getTotalNumberOfCustomers());
+        allCustomers.removeCustomer("#33");
+        assertEquals(3, allCustomers.getTotalNumberOfCustomers());
     }
 
     @Test
     public void getTotalSalesTest() {
+        ItemList storeItems = new ItemList();
+        for (int i = 1; i <= 5; i++) {
+            storeItems.addItem(new Item("toy#" + i, i, ""));
+        }
+        CustomerCart cart = new CustomerCart(storeItems);
 
+        for (int i = 1; i <= 5; i++) {
+            cart.addToCart("toy#" + i);
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            allCustomers.addCustomer(new Customer(i,
+                    "#" + i,
+                    "",
+                    0,
+                    cart));
+        }
+
+        assertEquals(75.0, allCustomers.getTotalSales());
     }
 
     @Test
     public void allCustomerNamesTest() {
-
+        for (int i = 1; i <= 3; i++) {
+            allCustomers.addCustomer(new Customer(i,
+                    "#" + i,
+                    "",
+                    0,
+                    new CustomerCart(new ItemList())));
+        }
+        assertEquals("#1 - #2 - #3 - ", allCustomers.allCustomerNames());
     }
 }
