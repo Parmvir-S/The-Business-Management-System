b5587b261322b25cb7c505d760fe33ea7d5c2005
Parmvir-S <parmvir.shergill@gmail.com>
2021-11-15 23:39:50 -0800

    implemented view store stats functionality

--- src/main/ui/StoreJPanels/StoreStatsDisplayPanel.java
+++ src/main/ui/StoreJPanels/StoreStatsDisplayPanel.java
@@ -14,6 +14,8 @@ public class StoreStatsDisplayPanel extends JPanel {
     private MainPanelContainer container;
     private ItemList storeItems;
     private AllCustomers allCustomers;
+    private JLabel totalCustomerLabel;
+    private JLabel totalSalesLabel;
 
     public StoreStatsDisplayPanel(MainPanelContainer container, ItemList storeItems, AllCustomers allCustomers) {
         this.storeItems = storeItems;
@@ -25,15 +27,42 @@ public class StoreStatsDisplayPanel extends JPanel {
     public void initialize() {
         setBackground(Color.cyan);
 
-        JButton button = new JButton("back");
-        button.addActionListener(new ActionListener() {
+        totalCustomerLabel = new JLabel();
+        totalSalesLabel = new JLabel();
+
+        add(totalCustomerLabel);
+        add(totalSalesLabel);
+
+        JButton getTotalButton = makeGetTotalButton();
+        add(getTotalButton);
+
+        JButton goBackButton = makeGoBackButton();
+        add(goBackButton);
+    }
+
+    public JButton makeGetTotalButton() {
+        JButton getTotalButton = new JButton("Total Customers/Sales");
+        getTotalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int totalCustomers = allCustomers.getTotalNumberOfCustomers();
+                double totalSales = allCustomers.getTotalSales();
+                totalCustomerLabel.setText("Total Customers: " + totalCustomers);
+                totalSalesLabel.setText("Total Sales: " + totalSales);
+            }
+        });
+        return getTotalButton;
+    }
+
+    public JButton makeGoBackButton() {
+        JButton goBackButton = new JButton("Go Back");
+        goBackButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cardLayout = (CardLayout) container.getLayout();
                 cardLayout.show(container, "storeMenu");
             }
         });
-
-        add(button);
+        return goBackButton;
     }
 }
